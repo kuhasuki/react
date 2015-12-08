ab9ce26ef656097a6e824f18c0418553adddcb08
App Academy Student <student@appacademy.io>
2015-12-07 17:02:34 -0800

    Tabs working

--- tabs.jsx
+++ tabs.jsx
@@ -5,14 +5,14 @@ var Tabs = React.createClass({
     return {content: ""};
   },
   getContent: function(e) {
-    this.setState({content: e.target.innerHTML});
+    this.setState({content: e.target.getAttribute('data-inside')});
   },
   render: function () {
     return(
       <div>
         {
           this.props.contents.map(function(entry){
-            return <h4 key={entry["title"]} onClick={this.getContent} >{entry['title']}</h4>;
+            return <h4 key={entry["title"]} data-inside={entry["content"]} onClick={this.getContent} >{entry['title']}</h4>;
           }.bind(this))
         }
         <p>{this.state.content}</p>
